5493d898e8975dd70b3c4b7c3c5b0ba762005969
ParvinaFS3 <parvinaisma@gmail.com>
2023-11-02 22:14:55 +0400

    first commit

---
+++ infosolution/ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "Suleyman Semed Tural Mirqubad";
+            string reversedString = ReverseString(inputString);
+
+            Console.WriteLine(reversedString);
+        }
+        // Verilen mətni ters çevirən  metod
+        static string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray(); 
+            int start = 0;
+            int end = input.Length - 1;
+
+            
+            while (start < end)
+            {
+                char temp = charArray[start];
+                charArray[start] = charArray[end];
+                charArray[end] = temp;
+                start++;
+                end--;
+            }
+
+            return new string(charArray); 
+        }
+
+    }
+}
